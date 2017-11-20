a2f6d3b2b5bede3477aa9c06bdb079c602ca4765
tabby42 <irene.theiss@gmx.at>
2017-11-21 00:48:42 +0100

    add some variables and comments

--- js/application.js
+++ js/application.js
@@ -2,14 +2,23 @@ var $j = jQuery.noConflict();
 
 $j(document).ready(function() {
 	"use strict";
-    //$j('#msg-board').hide();
+    //status
+    var strength = 10;
+    var hunger = 0;
+    var stepsTaken = 0;
     var currentRoom = "hallway";
     //items
     var sword, food, ring, armor, key = false;
-
+    //monsters
+    var evilKing, hellhound, poisonousBat;
+    //DOM elements
     var help = $j("#help");
-
+    
+    //hide help section initially
     help.hide();
+
+    //TODO: functions for:
+    //pickUp, eat, showInventory, useSpell, goToRoom, useKey, fight
 
     $j("form").submit(function () {
         console.log('submitted');
