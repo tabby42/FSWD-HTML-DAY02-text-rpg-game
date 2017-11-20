b5930106a0a8fde261e971f42a8781f97b8f1b98
tabby42 <irene.theiss@gmx.at>
2017-11-21 00:36:03 +0100

    add variables for items and help section, read user input on form submit, handle 'help' command

--- js/application.js
+++ js/application.js
@@ -3,5 +3,25 @@ var $j = jQuery.noConflict();
 $j(document).ready(function() {
 	"use strict";
     //$j('#msg-board').hide();
+    var currentRoom = "hallway";
+    //items
+    var sword, food, ring, armor, key = false;
+
+    var help = $j("#help");
+
+    help.hide();
+
+    $j("form").submit(function () {
+        console.log('submitted');
+        var input = $j("#user-input").val();
+
+        if (input == "help") {
+            help.show();
+        }
+
+
+        $j("user-input").val("");
+
+    });
 
 });
