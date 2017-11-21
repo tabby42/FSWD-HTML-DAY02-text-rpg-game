c3e147fb0ef2187e084c44ba515897f7c70fd24a
tabby42 <irene.theiss@gmx.at>
2017-11-21 17:24:32 +0100

    add player object

--- js/application.js
+++ js/application.js
@@ -1,10 +1,26 @@
 var room2 = {
-    "items": [],
-    "availableRooms": [
+    items: [],
+    availableRooms [
         [1, "w", false], 
         [3, "e", false]
     ]
 }
+
+var player = {
+    inventory: [],
+    strength: 10,
+    hunger: 0,
+    //showInventory
+    //goTo Room(direction)
+    //pickUp(item)
+    //fight(monster)
+    //useSpell()
+    //useKey()
+    //eat()
+
+}
+
+
 
 
 
@@ -18,18 +34,24 @@ $j(document).ready(function() {
     var strength = 10;
     var hunger = 0;
     var currentRoom = "room2";
+    var inventory = [];
     //items
-    var sword, food, ring, armor, key = false;
+    var sword, food, ring, armor, key;
     //monsters
     var evilKing, hellhound;
     //DOM elements
     var help = $j("#help");
 
+    //Functions
+    function insertMessage(message) {
+        
+    }
+
     //hide help section initially
     help.hide();
 
     //TODO: functions for:
-    //insertMessage, pickUp, eat, showInventory, useSpell, goToRoom(direction), useKey, fight(monster), checkHunger, checkStrength
+    //insertMessage, checkHunger, checkStrength
 
     $j("form").submit(function () {
         console.log('submitted');
